dd7d1ef05fe61b855d833b3dba229a9279fd9659
DanTiti <dans.ochoas@gmail.com>
2024-11-18 12:58:38 -0700

    camnios 1

---
+++ existente.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import customtkinter as ctk
+from bd import bd
+import traceback
+
+class Existente(ctk.CTk):
+    def __init__(self):
+        pass
